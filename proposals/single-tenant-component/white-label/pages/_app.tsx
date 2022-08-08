8d448e253c72cea12a519625ffc5cff65ea0d124
Ayoub Adib <adbayb@gmail.com>
2022-08-08 13:05:29 +0200

    fix(single-tenant-component): hydration error by inlining synchronously shell import (via require)

--- proposals/single-tenant-component/white-label/pages/_app.tsx
+++ proposals/single-tenant-component/white-label/pages/_app.tsx
@@ -1,15 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-/*
-// @ts-expect-error test
-import { Shell as RedShell } from "@single-tenant-component/shells/brand-red"
-*/
-import dynamic from 'next/dynamic';
-
-// @ts-expect-error
-const Shell = dynamic<any>(() => import(`@single-tenant-component/shells/brand-red`).then((mod) => mod.Shell), {
-  loading: () => <span>loading</span>,
-})
+const { Shell } = require(`@single-tenant-component/shells/${process.env.BRAND_ID}`)
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Shell><Component {...pageProps} /></Shell>;
