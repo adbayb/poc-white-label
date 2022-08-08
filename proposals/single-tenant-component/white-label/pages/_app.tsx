0a03336a83d5f3701b5e6c7f9dd5a0ea11605e3b
Ayoub Adib <adbayb@gmail.com>
2022-08-08 16:28:43 +0200

    fix(single-tenant-component): expose Shell typing white-label side

--- proposals/single-tenant-component/white-label/pages/_app.tsx
+++ proposals/single-tenant-component/white-label/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-const Shell = require(`@single-tenant-component/shells/${process.env.BRAND_ID}`).default
+const Shell = require(`@single-tenant-component/shells/${process.env.BRAND_ID}`).default as typeof import("@single-tenant-component/shells/brand-red").default
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Shell><Component {...pageProps} /></Shell>;
