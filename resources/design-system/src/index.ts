03bf4a1a961279de0cb37ed8b4ce5ffc9e177a48
Ayoub Adib <adbayb@gmail.com>
2022-08-17 09:44:08 +0200

    chore: watch framework packages

--- resources/design-system/src/index.ts
+++ resources/design-system/src/index.ts
@@ -2,5 +2,4 @@ export * from "./primitives/View";
 export * from "./primitives/Typography";
 export * from "./primitives/tokens";
 export * from "./components/Button";
-export * from "./components/Card";
 export * from "./Provider";
